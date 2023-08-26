b5d5eca90ba4309a4bc20f9beb3a79daf58a213f
suhas <suhasmanjunath2005.mail@gmail.com>
2023-08-26 15:15:38 +0530

    intial commit

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def get_todos(filepath=FILEPATH):
 
 
 def write_todos(todos_arg, filepath=FILEPATH):
-    """ Write the to-do items list in the text file."""
+    """ Write the to-do items list in the text file. and print"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
